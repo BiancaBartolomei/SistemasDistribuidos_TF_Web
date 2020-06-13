ac2448c63f2d22d39b73ec4248fe46987b041862
Bianca <biancarbartolomei@gmail.com>
2020-06-13 15:29:28 -0300

    [ADD] Main

--- src/Main.js
+++ src/Main.js
@@ -4,13 +4,13 @@ import login_img from './Images/Sistema.jpg'
 import ReactDOM from "react-dom";
 import './Login.css';
 import "antd/dist/antd.css";
-import { Row, Col, Form, Input, Button, Card, Layout, Menu } from 'antd';
+import { Row, Col, Form, Input, Button, Card, Layout, Menu, Table, Tag, Space } from 'antd';
 import {
     MenuUnfoldOutlined,
     MenuFoldOutlined,
     UserOutlined,
     VideoCameraOutlined,
-    UploadOutlined,
+    RightOutlined,
   } from '@ant-design/icons';
 const { Header, Sider, Content } = Layout;
 const layout = {
@@ -23,18 +23,70 @@ const tailLayout = {
 export default class Main extends React.Component {
   constructor() {
     super();
+
+
+
     this.state = {
         collapsed: false,
+        data: []
     }
   }
+
+  componentDidMount() {
+    fetch('http://localhost:3300/allRequests',{
+        method: 'GET'})
+        .then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson)
+          if(responseJson.length === 0){
+            this.setState({erro: "Usuário e/ou senha incorreta."})
+          } else {
+            this.setState({data: responseJson})
+            console.log('aaa')
+
+  
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
 
   toggle = () => {
     this.setState({
       collapsed: !this.state.collapsed,
     });
   };
+
+
 render(){
 
+    const columns = [
+        {
+          title: 'Nome',
+          dataIndex: 'name',
+          key: 'name',
+          render: text => <a>{text}</a>,
+        },
+        {
+          title: 'CNPJ',
+          dataIndex: 'cnpj',
+          key: 'cnpj',
+        },
+        {
+          title: 'Enviado por',
+          dataIndex: 'username',
+          key: 'username',
+        },
+        {
+          title: 'Action',
+          key: 'action',
+          render: (text, record) => (
+            <Button type="primary" shape="circle" icon={<RightOutlined />} onClick={(e) => {this.props.history.push({pathname:'/', record:record})}}/>
+          ),
+        },
+      ];
+      
 
   return (
     <>
@@ -58,7 +110,7 @@ render(){
               padding: 24,
             }}
           >
-            <Card/>
+            <Card><Table columns={columns} dataSource={this.state.data} /></Card>
           </Content>
         </Layout>
       </Layout>
